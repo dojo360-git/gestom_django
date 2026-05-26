624f14ff5606ab28f875d1416f5db11bcb958e28
Julien Besombes <besombes.julien@gmail.com>
2026-05-26 16:12:03 +0200

    20260526 1611

--- exports_csv_appli_access/migration_donnee_access.sql
+++ exports_csv_appli_access/migration_donnee_access.sql
@@ -10,7 +10,9 @@ SELECT *
 FROM core_heuresmanuelles;
 
 
-with agents as (
+with 
+
+agents as (
 	SELECT 
 		id, 
 		nom 
@@ -25,121 +27,155 @@ with agents as (
 		--arrivee, 
 		--depart, 
 		--tel
-	FROM public.core_agent ag 
-),
+	FROM public.core_agent ag ),
+	
 collectes as (
-SELECT  
-	id_collecte, 
-	date_collecte, 
-	a1_hr_debut, 
-	a1_hr_fin, 
-	a2_hr_debut, 
-	a2_hr_fin, 
-	a3_hr_debut, 
-	a3_hr_fin, 
-	motif_heures_sup, 
-	hr_sup_debut, 
-	hr_sup_fin, 
-	km_depart, 
-	km_retour, 
-	hr_depot_depart, 
-	hr_depot_retour, 
-	tonnage1, 
-	tonnage2, 
-	tonnage3, 
-	energie_qte_1, 
-	consignes, 
-	info_vehicule, 
-	info_collecte, 
-	date_creation, 
-	date_modification, 
-	id_agent_1_id, 
-	id_agent_2_id, 
-	id_agent_3_id, 
-	id_flux1_id, 
-	id_flux2_id, 
-	id_flux3_id, 
-	id_itineraire_id, 
-	id_vehicule_id
-FROM public.core_collecte
-),
+	SELECT  
+		id_collecte, 
+		date_collecte, 
+		a1_hr_debut, 
+		a1_hr_fin, 
+		a2_hr_debut, 
+		a2_hr_fin, 
+		a3_hr_debut, 
+		a3_hr_fin, 
+		motif_heures_sup, 
+		hr_sup_debut, 
+		hr_sup_fin, 
+		km_depart, 
+		km_retour, 
+		hr_depot_depart, 
+		hr_depot_retour, 
+		tonnage1, 
+		tonnage2, 
+		tonnage3, 
+		energie_qte_1, 
+		consignes, 
+		info_vehicule, 
+		info_collecte, 
+		date_creation, 
+		date_modification, 
+		id_agent_1_id, 
+		id_agent_2_id, 
+		id_agent_3_id, 
+		id_flux1_id, 
+		id_flux2_id, 
+		id_flux3_id, 
+		id_itineraire_id, 
+		id_vehicule_id
+	FROM public.core_collecte),
+	
+tb_planning as (
+	SELECT 
+		"IdPlan", 
+		"DatePlan", 
+		"NomAgentPlan" ag, 
+		"HFSAgentPlan", 
+		"HSupAgentPlan", 
+		"MotifHSPlan", 
+		"TRPlan", 
+		"MotifAbsPlan",
+		"HDebutPlan", 
+		"ObsPlan",
+		pm.id id_motif_pres,
+		ag.id id_agent,
+		"IdTourneePlan" id_collecte
+	FROM public."zzz_Tb_Planning" tbp
+	LEFT JOIN (
+		SELECT 
+		id, 
+		pres
+		--presence, 
+		--jour_travail, 
+		--couleur_hex_motif_presence, 
+		--date_creation, 
+		--date_modification
+		FROM public.core_presencemotif pm
+		) pm 
+	ON pm.pres = tbp."MotifAbsPlan"	
+	LEFT JOIN agents ag
+	ON ag.nom = tbp."NomAgentPlan"),
 
-planning as (
-SELECT 
-	"IdPlan", 
-	"DatePlan", 
-	"NomAgentPlan", 
-	"HFSAgentPlan", 
-	"HSupAgentPlan", 
-	"MotifHSPlan", 
-	"TRPlan", 
-	"MotifAbsPlan", 
-	"IdTourneePlan", 
-	"HDebutPlan", 
-	"ObsPlan"
-FROM public."zzz_Tb_Planning"
-),
 
-planning_tournees as (
-select * 
-from planning
-where is not null
-) ,
+tb_planning_abs AS (
+	SELECT 
+		--tbp.id,
+		"IdPlan", 
+		"DatePlan", 
+		ag , -- "NomAgentPlan", 
+		"HFSAgentPlan", 
+		"HSupAgentPlan", 
+		"MotifHSPlan", 
+		"TRPlan", 
+		"MotifAbsPlan", 
+		--"IdTourneePlan", 
+		"HDebutPlan", 
+		"ObsPlan" 
+	FROM tb_planning tbp
+	WHERE NOT  ("MotifAbsPlan" is null or "MotifAbsPlan" = '')),
+
+
+
+
+--planning_tournees as (
+--select * 
+--from planning
+--where is not null
+--) ,
 
 
 fx as (
-SELECT 
-	id_flux, 
-	flux 
-	--flux_long, 
-	--couleur_flux, 
-	--archive
-FROM public.core_flux)
+	SELECT 
+		id_flux, 
+		flux 
+		--flux_long, 
+		--couleur_flux, 
+		--archive
+	FROM public.core_flux),
 
 
 
+migr_heuresmanuelles as (
+	SELECT 
+		id, 
+		"date", 
+		heure_debut, 
+		heure_fin, 
+		motif_heures_sup, 
+		date_creation, 
+		date_modification, 
+		agent_id, 
+		presence_id
+	FROM public.preprod_core_heuresmanuelles
 
-/*
- 
-IdTou id_collecte, 
-DateTou date_collecte, 
-NumTou, 
-AmPmTou, 
-FluxTou, 
-ChauffeurTou, 
-Ripeur1Tou, 
-Ripeur2Tou, 
-VehiculeTou, 
-T1Tou, 
-T2Tou, 
-Champ1, 
-T3Tou, 
-CarburantTou, 
-QCarbTou, 
-QAdBlueTou, 
-KmTou, 
-HFSTou, 
-HSupTou, 
-MotifHSupTou, 
-ServiceTou
- */
+),
 
+
+
+migr_abs as (
+	select * 
+	from tb_planning
+	where not ("MotifAbsPlan" is null or "MotifAbsPlan" = '')
+),
+
+
+migr_collectes as (
 SELECT 
 	tbt."IdTou" id_collecte, 
 	tbt."DateTou" date_collecte, 
---	a1_hr_debut, 
---	a1_hr_fin, 
---	a2_hr_debut, 
---	a2_hr_fin, 
---	a3_hr_debut, 
---	a3_hr_fin, 
+	tbp1."HDebutPlan" a1_hr_debut, 
+	tbp1."HFSAgentPlan"	a1_hr_fin, 
+	tbp2."HDebutPlan"	a2_hr_debut, 
+	tbp2."HFSAgentPlan"	a2_hr_fin, 
+	tbp3."HDebutPlan"	a3_hr_debut, 
+	tbp3."HFSAgentPlan"	a3_hr_fin, 
 	tbt."MotifHSupTou"	motif_heures_sup, 
 	'12:00:00'::time hr_sup_debut, 
 	'12:00:00'::time + "HSupTou"::interval hr_sup_fin, 
 	0	km_depart, 
 	tbt."KmTou" km_retour, 
---	hr_depot_depart, 
---	hr_depot_retour, 
+	tbp1."HDebutPlan"	hr_depot_depart, 
+	tbp1."HFSAgentPlan"	hr_depot_retour, 
 	tbt."T1Tou" tonnage1, 
 	tbt."T2Tou" tonnage2, 
 	tbt."T3Tou" tonnage3, 
@@ -163,9 +199,13 @@ left join agents ag1 on ag1.nom = tbt."ChauffeurTou"
 left join agents ag2 on ag2.nom = tbt."Ripeur1Tou" 
 left join agents ag3 on ag3.nom = tbt."Ripeur2Tou" 
 left join fx  on fx.flux = tbt."FluxTou"
--- limit;
+left join tb_planning tbp1 on tbp1.id_collecte = tbt."IdTou" and tbp1.ag = tbt."ChauffeurTou"
+left join tb_planning tbp2 on tbp2.id_collecte = tbt."IdTou" and tbp2.ag = tbt."Ripeur1Tou" 
+left join tb_planning tbp3 on tbp3.id_collecte = tbt."IdTou" and tbp3.ag = tbt."Ripeur2Tou"
+)
 
 
+SELECT * 
 
-
+FROM  tb_planning
 ;
